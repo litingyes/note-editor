7620de642c3ad52149a163c435cfa0b29928eef7
liting <luz.liting@gmail.com>
2024-03-24 20:20:44 +0800

    docs: add transition for toggle theme

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,12 @@
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import { NodeEditor } from '@note-editor/vue-kit'
+import Layout from './Layout.vue'
 import '@note-editor/vue-kit/style.css'
 
 export default {
   extends: DefaultTheme,
+  Layout,
   enhanceApp({ app }) {
     app.component('NoteEditor', NodeEditor)
   },
